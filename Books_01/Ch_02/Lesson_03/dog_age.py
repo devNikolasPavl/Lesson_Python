22ffd1b8c262f04e890f4af13b6f1438daae91f8
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-18 12:26:29 +0300

    book_01, lesson_03

---
+++ Books_01/Ch_02/Lesson_03/dog_age.py
@@ -0,0 +1,20 @@
+# Существует несколько способов добавления функции int()
+
+# 1 способ
+# dog_name = input('Как зовут вашу собаку?')
+# dog_age = input('Сколько лет вашей собаке?')
+# dog_age = int(dog_age)
+# human_age = dog_age * 7
+# print(human_age)
+
+# 2 способ
+# dog_name = input('как зовут вашу собаку?')
+# dog_age = input('сколько лет вашей собаке?')
+# human_age = int(dog_age) * 7
+# print(human_age)
+
+# 3 способ
+dog_name = input('Как зовут вашу собаку?')
+dog_age = int(input('Сколько лет вашей собаке?'))
+human_age = dog_age * 7
+print(human_age)
