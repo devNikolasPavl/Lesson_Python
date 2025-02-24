b7f066538a55b35730f85ba8b4a972fbb9785a40
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2025-02-24 10:06:36 +0300

    lesson_03

---
+++ Skillfactor/project_01/chapter_01/lesson_03/main.py
@@ -0,0 +1,7 @@
+# создайте переменную birth_year и присвойте ей ваш год рождения
+# вычислите ваш возраст и сохраните его в переменную current_age
+
+birth_year = 1980
+current_age = 2025 - birth_year
+print(current_age)
+
