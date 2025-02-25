a14b011cfb3fa0ce34f08eb24641b2c0a9deb625
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2025-02-25 10:33:24 +0300

    Смена файловых папок

---
+++ javarush/chapter_02/lesson_01/main.py
@@ -0,0 +1,9 @@
+# создаем переменные
+
+# создай переменные с именами city, age и country
+# присвоить им значения "London", 21 и "Япония"
+
+city = 'London'
+age = 21
+country = 'Япония'
+print(city, country, age)
